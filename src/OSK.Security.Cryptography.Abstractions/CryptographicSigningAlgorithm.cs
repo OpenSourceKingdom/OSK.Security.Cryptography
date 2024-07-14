e9bdf4b741b4aa7fab96f2a13fe57b6a7741524f
M <mmmasent@gmail.com>
2024-07-13 23:37:19 -0400

    Add better data signing handling (OSK-13)
    Motivation
    ----
    The library is heavily integrated with HashAlgorithmNames for signing algorithms, but this does not work for some AsymmetricKeyService's that do not use a classic signing hash algorithm
    
    Modifications
    ----
    * [Breaking Change] - converted IAsymmetricKeyService to using new `CryptographicSigningAlgorithm` that can be extended by implementing Cryptography key service packages to special CryptographicSigningAlgorithms that they support
    
    Result
    ----
    There is a more flexible way to separate supported signing algorithms from a hardset list of hash algorithms

---
+++ src/OSK.Security.Cryptography.Abstractions/CryptographicSigningAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OSK.Security.Cryptography.Abstractions
+{
+    public class CryptographicSigningAlgorithm
+    {
+        #region Variables
+
+        public string AlgorithmName { get; }
+
+        #endregion
+        
+        #region Constructors
+        
+        public CryptographicSigningAlgorithm(string algorithmName)
+        {
+            AlgorithmName = string.IsNullOrWhiteSpace(algorithmName)
+                ? throw new ArgumentNullException(nameof(algorithmName))
+                : algorithmName;
+        }
+        
+        #endregion
+    }
+}
