0363b61e53e67d5d0d71deab9dba0b59ba5debbd
M <mmmasent@gmail.com>
2024-08-09 21:33:11 -0400

    Adjust CryptographicSigningAlgorithm (OSK-15)
    Motivation
    ----
    The CryptographicSigningAlgorithm is a class when it could be a struct
    
    Modifications
    ----
    * Changed the type
    
    Result
    ----
    THe CryptographicSigningAlgorithm is more appropriately a struct

---
+++ src/OSK.Security.Cryptography.Abstractions.UnitTests/CryptographicSigningAlgorithmTests.cs
@@ -0,0 +1,109 @@
+﻿using Xunit;
+
+namespace OSK.Security.Cryptography.Abstractions.UnitTests
+{
+    public class CryptographicSigningAlgorithmTests
+    {
+        #region Equals
+
+        [Theory]
+        [InlineData("World", "Hello")]
+        [InlineData("world", "World")]
+        public void Equals_ObjectsNotEqual_ReturnsFalse(string algName1, string algName2)
+        {
+            // Arrange
+            var algorithm1 = new CryptographicSigningAlgorithm(algName1);
+            var algorithm2 = new CryptographicSigningAlgorithm(algName2);
+
+            // Act
+            var result = algorithm1 == algorithm2;
+            var result2 = algorithm1.Equals(algorithm2);
+
+            // Assert
+            Assert.False(result);
+            Assert.False(result2);
+        }
+
+        [Fact]
+        public void Equals_DefaultObjectDoesNotEqualCustomObject_ReturnsFalse()
+        {
+            // Arrange
+            var algorithm1 = new CryptographicSigningAlgorithm();
+            var algorithm2 = new CryptographicSigningAlgorithm("alg");
+
+            // Act
+            var result = algorithm1 == algorithm2;
+            var result2 = algorithm1.Equals(algorithm2);
+
+            // Assert
+            Assert.False(result);
+            Assert.False(result2);
+        }
+
+        [Fact]
+        public void Equals_ObjectsEqual_ReturnsTrue()
+        {
+            // Arrange
+            var algorithm1 = new CryptographicSigningAlgorithm("Hello");
+            var algorithm2 = new CryptographicSigningAlgorithm("Hello");
+
+            // Act
+            var result = algorithm1 == algorithm2;
+            var result2 = algorithm1.Equals(algorithm2);
+
+            // Assert
+            Assert.True(result);
+            Assert.True(result2);
+        }
+
+        [Fact]
+        public void Equals_DefaultObjectsEqual_ReturnsTrue()
+        {
+            // Arrange
+            var algorithm1 = new CryptographicSigningAlgorithm();
+            var algorithm2 = new CryptographicSigningAlgorithm();
+
+            // Act
+            var result = algorithm1 == algorithm2;
+            var result2 = algorithm1.Equals(algorithm2);
+
+            // Assert
+            Assert.True(result);
+            Assert.True(result2);
+        }
+
+        #endregion
+
+        #region DoesNotEqual Operator
+
+        [Fact]
+        public void DoesNotEqualOperator_ObjectsEqual_ReturnsFalse()
+        {
+            // Arrange
+            var algorithm1 = new CryptographicSigningAlgorithm();
+            var algorithm2 = new CryptographicSigningAlgorithm();
+
+            // Act
+            var result = algorithm1 != algorithm2;
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void DoesNotEqualOperator_ObjectsDoNotEqual_ReturnsTrue()
+        {
+            // Arrange
+            var algorithm1 = new CryptographicSigningAlgorithm("Hello");
+            var algorithm2 = new CryptographicSigningAlgorithm();
+
+            // Act
+            var result = algorithm1 != algorithm2;
+
+            // Assert
+            Assert.True(result);
+        }
+
+        #endregion
+    }
+}
--- src/OSK.Security.Cryptography.Abstractions/CryptographicSigningAlgorithm.cs
+++ src/OSK.Security.Cryptography.Abstractions/CryptographicSigningAlgorithm.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace OSK.Security.Cryptography.Abstractions
 {
-    public class CryptographicSigningAlgorithm
+    public readonly struct CryptographicSigningAlgorithm : IEquatable<CryptographicSigningAlgorithm>
     {
         #region Variables
 
@@ -18,7 +19,40 @@ namespace OSK.Security.Cryptography.Abstractions
                 ? throw new ArgumentNullException(nameof(algorithmName))
                 : algorithmName;
         }
-        
+
+        #endregion
+
+        #region IEquatable
+
+        public bool Equals(CryptographicSigningAlgorithm other)
+        {
+            return string.Equals(AlgorithmName, other.AlgorithmName, StringComparison.Ordinal);
+        }
+
+        public override int GetHashCode()
+        {
+            return AlgorithmName == null ? 0 : AlgorithmName.GetHashCode();
+        }
+
+        public override bool Equals([NotNullWhen(true)] object obj)
+        {
+            return obj is CryptographicSigningAlgorithm && Equals((CryptographicSigningAlgorithm)obj);
+        }
+
+        #endregion
+
+        #region Operators
+
+        public static bool operator ==(CryptographicSigningAlgorithm left, CryptographicSigningAlgorithm right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(CryptographicSigningAlgorithm left, CryptographicSigningAlgorithm right)
+        {
+            return !(left == right);
+        }
+
         #endregion
     }
 }
